5f8fa37ac725d9bb41f703868882af9398c12b48
yaolei <373561022@qq.com>
2018-03-04 16:06:10 +0800

    延迟加载

--- MyBatisCascadeExe/src/com/mybatis/cascade/exe/main/MainTest.java
+++ MyBatisCascadeExe/src/com/mybatis/cascade/exe/main/MainTest.java
@@ -1,6 +1,7 @@
 package com.mybatis.cascade.exe.main;
 
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 
 import com.mybatis.cascade.exe.mapper.EmployeeMapper;
 import com.mybatis.cascade.exe.pojo.Employee;
@@ -8,11 +9,33 @@ import com.mybatis.cascade.exe.utils.SqlSessionFactoryUtils;
 
 public class MainTest {
 
+	static Logger sLogger = Logger.getLogger(MainTest.class);
 	
 	public static void main(String[] args) {
 		SqlSession session = SqlSessionFactoryUtils.openSession();
 		EmployeeMapper employeeMapper = session.getMapper(EmployeeMapper.class);
 		Employee emp = employeeMapper.getEmployeeById(2l);
-		System.err.println(emp);
+//		sLogger.info("=============================================");
+//		try {
+//			Thread.sleep(2000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+		System.err.println(emp.getEmail());
+//		sLogger.info("=============================================");
+////		try {
+////			Thread.sleep(2000);
+////		} catch (InterruptedException e) {
+////			e.printStackTrace();
+////		}
+//		emp.getEmployeeTaskList().get(0);
+//		
+//		try {
+//		Thread.sleep(2000);
+//	} catch (InterruptedException e) {
+//		e.printStackTrace();
+//	}
+//		emp.getEmployeeTaskList().get(0).getTask();
+		System.err.println();
 	}
 }
