16cc15c7dcccec80d470989fbd5c117687a36f9d
yaolei <373561022@qq.com>
2018-03-08 20:55:13 +0800

    1

--- MyBatisPlugin/src/com/mybatis/plugin/main/MainTest.java
+++ MyBatisPlugin/src/com/mybatis/plugin/main/MainTest.java
@@ -1,11 +1,9 @@
 package com.mybatis.plugin.main;
 
-import java.util.List;
-
+import org.apache.commons.codec.binary.Base64;
 import org.apache.ibatis.session.SqlSession;
 
 import com.mybatis.plugin.mapper.UserMapper;
-import com.mybatis.plugin.pojo.PageParams;
 import com.mybatis.plugin.pojo.SexEnum;
 import com.mybatis.plugin.pojo.User;
 import com.mybatis.plugin.utils.SqlSessionFactoryUtils;
@@ -21,17 +19,23 @@ public class MainTest {
 //		System.out.println(list.size());
 		
 		
-		SqlSession session = SqlSessionFactoryUtils.openSession();
-		UserMapper userMapper = session.getMapper(UserMapper.class);
-		PageParams pageParam = new PageParams();
-		pageParam.setPage(11);
-		pageParam.setPageSize(5);
-		pageParam.setCleanOrderBy(true);
-		List<User> list = userMapper.findWithCondition(200l, 1l, pageParam);
-		System.out.println(list.size());
-		System.out.println(list);
-		System.out.println(pageParam.getTotalPage());
-		
+//		SqlSession session = SqlSessionFactoryUtils.openSession();
+//		UserMapper userMapper = session.getMapper(UserMapper.class);
+//		PageParams pageParam = new PageParams();
+//		pageParam.setPage(1);
+//		pageParam.setPageSize(5);
+//		pageParam.setCleanOrderBy(true);
+//		List<User> list = userMapper.findWithCondition(200l, 1l, pageParam);
+//		System.out.println(list.size());
+//		System.out.println(list);
+//		System.out.println(pageParam.getTotalPage());
+		Base64 base64 = new Base64();
+		String encode = base64.encodeToString("中华人民共和国".getBytes());
+		System.out.println(encode);
+		System.out.println(Base64.encodeBase64String("中华人民共和国".getBytes()));
+		byte[] data = Base64.decodeBase64(encode);
+		System.out.println(new String(base64.decode(encode)));
+		System.out.println(new String(data));
 	}
 	
 	
